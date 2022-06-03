6d3ce21c96ec34c3e72d1fa7b8966d476d61cb97
Kirill Zviahin <kira225@yahoo.com>
2022-06-03 15:36:46 +0300

    update ci

--- backend/src/gmop/authentication/tests.py
+++ backend/src/gmop/authentication/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .jwt import create_token
 
-# Create your tests here.
+
+class TestCase1(TestCase):
+    def test_missing_argument_for_token_creating(self):
+        print('!!!')
+        self.assertRaises(ValueError, create_token, username=None)
