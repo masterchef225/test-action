28ebbce1be7ad1f88cb5a947c75eda0dc657ef3f
Kirill Zviahin <kira225@yahoo.com>
2022-06-03 13:23:40 +0300

    update ci

--- gmop_data_init_0000.sql
+++ gmop_data_init_0000.sql
@@ -1,3 +1,11 @@
-CREATE DATABASE gmop;
-GRANT ALL PRIVILEGES ON gmop.* to gmop;
+-- CREATE DATABASE gmop;
+-- GRANT ALL PRIVILEGES ON gmop.* to gmop;
 
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255) 
+);
